707b4a9030fb7aa56db1945ac77ab0fb715c261f
akash yadav <64635004+akash4142@users.noreply.github.com>
2023-01-07 23:15:07 +0530

    Add files via upload

---
+++ w1p1.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+/*/< assessment name example : Workshop - #1 (Part - 1) >
+Full Name :akash yadav
+Student ID# :172566218
+	Email :ayadav35@myseneca.ca
+	Section :	NII
+	Authenticity Declaration :
+I declare this submission is the result of my own work and has not been
+shared with any other student or 3rd party content provider.This submitted
+piece of work is entirely of my own creation.*/
+int main()
+{
+	printf("Workshop 1 Part-1\n");
+	printf("=================\n\n");
+	printf("I'm displaying this using the 'printf' stdio\n");
+
+	printf("(standard input output) library function!\n\n");
+
+	printf("Dear teacher,\n\n");
+
+	printf("  I promise I will work hard from this day onward.\n");
+	printf("  I acknowledge that practice is extremely important,\n");
+	printf("  so I will do all workshops, quizzes, and assignments.\n\n");
+	printf("Sincerely,\n\n");
+	printf("AKASH YADAV\n");
+	printf("172566218");
+		return 0;
+}
